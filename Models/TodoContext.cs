5cddd44debefee807429995da466d7aefbb6e9a1
Егор Калмаков <132436476+Redshoulder001@users.noreply.github.com>
2023-05-15 12:47:02 +0500

    Add files via upload

---
+++ Models/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TodoApi.Models
+{
+    public class TodoContext:DbContext
+    {
+      public DbSet<TodoItem> TodoItems { get; set; }
+        
+    }
+
+}
